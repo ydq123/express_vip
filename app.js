4bdb0ec7ce7461644d2a500788260b9ef783aa69
ydq123 <30288223+ydq123@users.noreply.github.com>
2020-09-16 20:06:18 +0800

    Update app.js

--- app.js
+++ app.js
@@ -9,12 +9,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 //设置允许跨域访问该服务.
 app.all("*", function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  //Access-Control-Allow-Headers ,可根据浏览器的F12查看,把对应的粘贴在这里就行
-  // res.header('Access-Control-Allow-Headers', 'Content-Type');
-  res.header("Access-Control-Allow-Methods", "*");
-  // res.header('Content-Type', 'application/json;charset=utf-8');
-  next();
+ 	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type");
+	res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+  	next();
 });
 
 // 路由
